e2924a75a54c1cfa3d6b4c5612876db609850519
Your Nameadamjab78 <adamilpapnt@gmail.com>
2021-07-13 17:17:01 +0800

    perubahan 8

--- components/Footer.js
+++ components/Footer.js
@@ -1,12 +1,19 @@
 import { StatusBar } from "expo-status-bar";
 import React from "react";
 import { StyleSheet, Text, View } from "react-native";
+import { Ionicons }  from "@expo/vector-icons";
+import { MaterialCommunityIcons } from '@expo/vector-icons';
 
 const Footer = () => {
     return (
       <View style={{ flex: 1, flexDirection: "row" }}>
-        <View style={{ flex: 1, backgroundColor: "chocolate" }}></View>
-        <View style={{ flex: 1, backgroundColor: "darkviolet" }}></View>
+        <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+      <Ionicons name="home" size={32} color="black"></Ionicons>
+      </View>
+    
+        <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+      <MaterialCommunityIcons name="face-profile" size={32} color="black"></MaterialCommunityIcons>
+      </View>
       </View>
     );
   };
--- components/Header.js
+++ components/Header.js
@@ -1,14 +1,20 @@
 import { StatusBar } from "expo-status-bar";
 import React from "react";
 import { StyleSheet, Text, View } from "react-native";
+import {Ionicons} from "@expo/vector-icons";
+import { MaterialCommunityIcons } from '@expo/vector-icons';
 
 const Header = () => {
   return (
     <View style={{ flex: 1, flexDirection: "row" }}>
       <View style={{ flex: 1, backgroundColor: "green" }}></View>
-      <View style={{ flex: 2, backgroundColor: "dimgrey" }}></View>
-      <View style={{ flex: 1, backgroundColor: "goldenrod" }}></View>
+      <View style={{flex: 2, backgroundColor: "dimgrey",justifyContent: "center", alignItems: "center",}}>
+        <Text style={{fontweight: "bold", fontsize: 32, color: "white" }}>HOME</Text>
+      </View>
+      <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+      <MaterialCommunityIcons name="face-profile" size={32} color="black"></MaterialCommunityIcons>
+      </View>
     </View>
   );
 };
- export default Header;
+export default Header;
