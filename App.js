e80a466e2773682005071349d556ca92ac209420
Your Nameadamjab78 <adamilpapnt@gmail.com>
2021-07-13 10:45:53 +0800

    Perubahan 3

--- App.js
+++ App.js
@@ -4,25 +4,5 @@ import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Nama Saya Adam</Text>
-      <Text>Saya Budak baru belajar</Text>
-      <Text>Alhamdulillah Berjaya Akhirnya</Text>
-      <Text>................................</Text>
-      <Text>ok hari ni dah faham sikit</Text>
-      <Text>                          </Text>
-      <Text>Hooraaaa...</Text>
-      <Text>Hoorayyy...</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+  <View style={{ flex: 1, backgroundColor: "blue" }}></View>
+)}
