e59f6aa6a2b1228174b87d67f9c0d66892848ed3
Your Nameadamjab78 <adamilpapnt@gmail.com>
2021-07-13 12:08:40 +0800

    perubahan 5

--- App.js
+++ App.js
@@ -1,8 +1,24 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
 
 export default function App() {
   return (
-  <View style={{ flex: 1, backgroundColor: "blue" }}></View>
-)}
+    <View style={{ flex: 1 }}>
+      <View style={{ flex: 1, flexDirection: "row" }}>
+        <View style={{ flex: 1, backgroundColor: "yellow" }}></View>
+        <View style={{ flex: 1, backgroundColor: "cyan" }}></View>
+        <View style={{ flex: 1, backgroundColor: "orange" }}></View>
+      </View>
+
+      <View style={{ flex: 5 }}>
+        <View style={{ flex: 1, backgroundColor: "lightgrey" }}></View>
+      </View>
+
+      <View style={{ flex: 1, flexDirection: "row" }}>
+        <View style={{ flex: 1, backgroundColor: "blue" }}></View>
+        <View style={{ flex: 1, backgroundColor: "magenta" }}></View>
+      </View>
+    </View>
+  );
+}
