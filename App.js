9b839494aef0e21ed9fbc112a8f1c1d73c9f6b6e
Your Nameadamjab78 <adamilpapnt@gmail.com>
2021-07-13 09:29:32 +0800

    Perubahan 1st

--- App.js
+++ App.js
@@ -5,7 +5,13 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Nama Saya Adam</Text>
+      <Text>Saya Budak baru belajar</Text>
+      <Text>Alhamdulillah Berjaya Akhirnya</Text>
+      <Text>................................</Text>
+      <Text>ok hari ni dah faham sikit</Text>
+      <Text>                          </Text>
+      <Text>Hoorayyy...</Text>
       <StatusBar style="auto" />
     </View>
   );
